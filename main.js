15baa030bbc0288b89f70ae48e4d54d0458ca1cd
Aramuad <samcballard@gmail.com>
2018-08-22 18:59:26 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("rock").addEventListener("click", function() {
+
+})
+document.getElementById("paper").addEventListener("click", )
+document.getElementById("scissors").addEventListener("click", )
+
+
+// Spent a lot of the day trying to square away my html and css
+// Spent the rest of the day doing javascript tutorials because I don't think I know enough to work on the assignments
